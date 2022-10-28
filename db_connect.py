d5582cf63fc26f2e2eddad3186e34fdd202ee1af
snhou-ubuntu <shawnhouforwork@gmail.com>
2022-10-28 23:59:21 +0800

    connect database

--- db_connect.py
+++ db_connect.py
@@ -1,16 +1,38 @@
-from mysql.connector.constants import ClientFlag
+import json
 import mysql
-try:
-    sql_config = {}
+import mysql.connector
 
+with open("config.json") as json_file:
+    sql_config = json.load(json_file)
+
+query = """
+        SELECT *
+        FROM books
+        """
+
+try:
+    """ connect """
     cnxn = mysql.connector.connect(**sql_config)
+    cursor = cnxn.cursor()
     if cnxn.is_connected():
         cursor = cnxn.cursor()
         print("connection success")
+
+        """ execute query """
+        cursor.execute(query)
+        
+        """ get result """
+        get_data = cursor.fetchall()
+
 except Exception as e:
-    print("資料庫連接失敗：", e)
+    print("error:", e,"\n")
+
 finally:
     if (cnxn.is_connected()):
+        """ close connect """
         cursor.close()
         cnxn.close()
-        print("資料庫連線已關閉")
+        print("close")
+
+
+print(get_data)
